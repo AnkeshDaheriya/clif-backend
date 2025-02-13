29ac08ee78c5853f5776dd59993cf19a04513519
Nitin Jain <nitinjn07@gmail.com>
2025-02-13 14:11:12 +0530

    Done

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -1,32 +1,43 @@
 const { textExtraction } = require("../helper/resumeTextParser.js");
+const { AIResume } = require("../helper/OpenAiHelper.js");
+const { promptFormat } = require("../config/prompt.js");
 // add comment to push code
 const resumeUpload = async (req, res) => {
-  if (!req.files) {
-    return {
+  if (!req.file) {
+    return res.json({
       status: 400,
       message: "No file was uploaded!",
       success: false,
-    };
+    });
   }
-
-  const fileLocation = `public/resume_files/${req.files.fileUpload[0].originalname}`; // Add a slash before the filename
+  // console.log(req.file);
+  const fileLocation = public/resume_files/${req.file.originalname}; // Add a slash before the filename
   // console.log("$fileName", fileLocation);
 
   try {
     const extractedText = await textExtraction(fileLocation);
     // console.log(extractedText);
-    return {
+    const prompt = `Extract all the key details from resume text ${extractedText?.pages} 
+                    and give all the resume details in ${promptFormat} in json data keep all technical skills in a array and 
+                    non technical skills in a array and all other skills in a array all within skills section  `;
+
+    const response = await AIResume(prompt);
+
+    const J_data = await JSON.parse(response);
+    console.log("resume starts here");
+    console.dir(J_data, { depth: null });
+    return res.json({
       message: "Extracted text",
-      extractedText: extractedText,
+      extractedText: J_data,
       success: true,
-    };
+    });
   } catch (error) {
     console.error("Error extracting text:", error);
-    return {
+    return res.json({
       status: 500,
       message: "Error extracting text",
       success: false,
-    };
+    });
   }
 };
 
--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,7 +1,7 @@
 const exp = require("express");
 const router = exp.Router();
 const { upload } = require("../helper/pdfUpload");
-
-// router.post("/upload", upload.single("resume"), resume.uploadResume);
+const { uploadResume } = require("../controllers/resumeController");
+router.post("/upload", upload.single("resume"), uploadResume);
 
 module.exports = router;
