4cc202f2bed34ba757f5f02eff59f2fe5326bb04
Nitin Jain <nitinjn07@gmail.com>
2025-02-14 16:30:49 +0530

    done

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-const exp = require("express");
-const router = exp.Router();
-const { upload } = require("../helper/pdfUpload");
-const { uploadResume } = require("../controllers/resumeController");
-router.post("/upload", upload.single("resume"), uploadResume);
-=======
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../helper/pdfUpload"); // Assuming this is the file handling logic
@@ -12,6 +5,5 @@ const { singleResumeUpload } = require("../controllers/singleResumeController");
 
 // POST route to handle resume upload
 router.post("/upload", upload.single("resume"), singleResumeUpload);
->>>>>>> 90fda61a17782cb4efd37b80fc56526df05a60fc
 
 module.exports = router;
