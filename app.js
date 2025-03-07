66a0074ccab79294363fb999151c8c0f2db737cc
Nitin Jain <nitinjn07@gmail.com>
2025-03-07 16:54:27 +0530

    done

--- app.js
+++ app.js
@@ -94,11 +94,11 @@ const upload = multer({
 const allowedOrigins =
   process.env.NODE_ENV === "production"
     ? [
-      "https://uat.clif.ai",
-      "https://clif.ai",
-      "https://www.uat.clif.ai",
-      "https://www.clif.ai",
-    ]
+        "https://uat.clif.ai",
+        "https://clif.ai",
+        "https://www.uat.clif.ai",
+        "https://www.clif.ai",
+      ]
     : ["http://localhost:3000", "http://localhost:3001"]; // Development origins
 
 app.use(
@@ -186,7 +186,10 @@ app.use("/api/progress", progressRoutes);
 app.use("/admin/mentors", assignMentorRoutes);
 // mentor routes
 app.use("/mentor/auth", require("./routes/mentor/authRoutes.js"));
-app.use("/mentor/availability", require("./routes/mentor/availabilityRoutes.js"));
+app.use(
+  "/mentor/availability",
+  require("./routes/mentor/availabilityRoutes.js")
+);
 app.use("/mentor/booking", require("./routes/mentor/bookingRoutes.js"));
 
 // event routes
